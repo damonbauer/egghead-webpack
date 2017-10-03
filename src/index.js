d51a23144995ee85a87202da1f2a373505007d21
Damon Bauer <hello@damonbauer.me>
2017-10-03 05:58:15 -0500

    Adds HTML injection, styles, CSS-* loaders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import getRgb from './color/getRgb';
 import rgbToHex from './color/rgbToHex';
 
+import './index.scss';
+
 function getHex(opts, inputs) {
   const rgb = getRgb(opts, inputs);
   return rgbToHex(rgb);
 }
 
-document.getElementById('content').textContent = getHex();
+const content = document.getElementById('content');
+
+let i = 0;
+
+while (i<5) {
+  const color = getHex();
+  const markup = `
+    <div class="card" style="background-color: ${color}">
+      <strong>${color}</strong>
+    </div>
+  `;
+  content.innerHTML += markup;
+  i++;
+}
