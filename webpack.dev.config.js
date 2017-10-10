ba5b510ae5b9c338c27c71d5dd48c7973d9cde67
Damon Bauer <hello@damonbauer.me>
2017-10-10 05:55:23 -0500

    Adds dev server config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,5 +3,13 @@ var merge = require('webpack-merge');
 var baseConfig = require('./webpack.base.config');
 
 module.exports = merge.smart(baseConfig, {
-  devtool: 'eval-source-map'
+  devtool: 'eval-source-map',
+  devServer: {    
+    contentBase: './dist',    
+    overlay: true,    
+    open: true,   
+    watchOptions: {   
+      ignored: /node_modules/   
+    }   
+  }
 });
