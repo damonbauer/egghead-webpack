683582378cbf483d58505802e32d347fe6a61cbe
Damon Bauer <hello@damonbauer.me>
2017-10-10 05:13:58 -0500

    Adds webpack-dev-server, updates dev config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,5 +6,13 @@ module.exports = merge.smart(baseConfig, {
   devtool: 'eval-source-map',
   output: {
     filename: 'app-dev-[hash]-[id].js'
+  },
+  devServer: {
+    contentBase: './dist',
+    overlay: true,
+    open: true,
+    watchOptions: {
+      ignored: /node_modules/
+    }
   }
 });
