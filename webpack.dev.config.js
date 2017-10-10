c3ce30df3b1feb82bd7494d0fa01bc2187ec2e9d
Damon Bauer <damonbauer@users.noreply.github.com>
2017-10-10 05:53:26 -0500

    Lesson 07 sourcemaps (#10)

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,16 +3,5 @@ var merge = require('webpack-merge');
 var baseConfig = require('./webpack.base.config');
 
 module.exports = merge.smart(baseConfig, {
-  devtool: 'eval-source-map',
-  output: {
-    filename: 'app-dev-[hash]-[id].js'
-  },
-  devServer: {
-    contentBase: './dist',
-    overlay: true,
-    open: true,
-    watchOptions: {
-      ignored: /node_modules/
-    }
-  }
+  devtool: 'eval-source-map'
 });
